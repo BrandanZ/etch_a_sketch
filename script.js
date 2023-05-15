fcea8ad0d8f763464c0bc8e08793a73bde47dd36
brandon <branthebuilder321@gmail.com>
2023-05-15 19:32:34 -0400

    Set up basics before grid

---
+++ script.js
@@ -0,0 +1,5 @@
+const container = document.querySelector('.container');
+const newDiv = document.createElement("div");
+newDiv.classList.add('gridContainer');
+newDiv.textContent = 'test';
+container.appendChild(newDiv);
